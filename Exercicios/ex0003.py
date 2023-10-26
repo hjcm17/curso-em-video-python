5ad4a15a3234a75e643c130ee54674e8868946c6
Hugo Mota <hugojcmota@gmail.cm>
2023-10-26 19:11:00 -0300

    ex001-003 commit

---
+++ Exercicios/ex0003.py
@@ -0,0 +1,8 @@
+# Desafio 003
+# Crie um programa que leia dois números e mostre a soma entre eles
+print('Esse programa realiza a soma entre dois números...')
+# colocar o tipo primitivo antes do input
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+soma = num1+num2
+print('{} + {} = {}'.format(num1,num2,soma))
---
+++ Exercicios/ex001.py
@@ -0,0 +1,5 @@
+# Desafio 001
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+#print("Olá, Mundo!") # primeira maneira
+msg = "Olá, Mundo!" # segunda maneira - atribuindo um valor a variável
+print(msg)
